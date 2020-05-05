d28ffd64de3b3c26feef7bd2f1c23dbf3b60c2cd
rjayram96 <rjayram96@gmail.com>
2020-05-05 23:32:29 +0530

    code refactoring done

--- ClientApplication/WebContent/Components/items.js
+++ ClientApplication/WebContent/Components/items.js
@@ -1,10 +1,10 @@
 $(document).ready(function()
 {
-if ($("#alertSuccess").text().trim() == "")
- {
- $("#alertSuccess").hide();
- }
- $("#alertError").hide();
+	if ($("#alertSuccess").text().trim() == "")
+	 {
+		$("#alertSuccess").hide();
+	 }
+	 $("#alertError").hide();
 });
 
 //SAVE ============================================
@@ -12,31 +12,34 @@ $(document).on("click", "#btnSave", function(event)
 {
 	var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT";
 	$.ajax(
-			{
-			 url : "ItemsAPI",
-			 type : type,
-			 data : $("#formItem").serialize(),
-			 dataType : "text",
-			 complete : function(response, status)
-			 {
+	{
+		 url : "ItemsAPI",
+		 type : type,
+		 data : $("#formItem").serialize(),
+		 dataType : "text",
+		 complete : function(response, status)
+		 {
 			 onItemSaveComplete(response.responseText, status);
-			 }
-			});
-// Clear alerts---------------------
- $("#alertSuccess").text("");
- $("#alertSuccess").hide();
- $("#alertError").text("");
- $("#alertError").hide(); 
-//Form validation-------------------
- var status = validateItemForm();
- if (status != true)
-  {
-  $("#alertError").text(status);
-  $("#alertError").show();
-  return;
-  }
- // If valid-------------------------
-  $("#formItem").submit();
+		 }
+	});
+		
+	// Clear alerts---------------------
+	 $("#alertSuccess").text("");
+	 $("#alertSuccess").hide();
+	 $("#alertError").text("");
+	 $("#alertError").hide(); 
+	 
+	//Form validation-------------------
+	 var status = validateItemForm();
+	 if (status != true)
+	  {
+		  $("#alertError").text(status);
+		  $("#alertError").show();
+		  return;
+	  }
+	 
+	 // If valid-------------------------
+	  $("#formItem").submit();
   
   
  });
@@ -44,45 +47,51 @@ $(document).on("click", "#btnSave", function(event)
 //UPDATE==========================================
 $(document).on("click", ".btnUpdate", function(event)
 {
- $("#hidItemIDSave").val($(this).closest("tr").find('#hidItemIDUpdate').val());
- $("#itemCode").val($(this).closest("tr").find('td:eq(0)').text());
- $("#itemName").val($(this).closest("tr").find('td:eq(1)').text());
- $("#itemPrice").val($(this).closest("tr").find('td:eq(2)').text());
- $("#itemDesc").val($(this).closest("tr").find('td:eq(3)').text());
+	 $("#hidItemIDSave").val($(this).closest("tr").find('#hidItemIDUpdate').val());
+	 $("#itemCode").val($(this).closest("tr").find('td:eq(0)').text());
+	 $("#itemName").val($(this).closest("tr").find('td:eq(1)').text());
+	 $("#itemPrice").val($(this).closest("tr").find('td:eq(2)').text());
+	 $("#itemDesc").val($(this).closest("tr").find('td:eq(3)').text());
 });
 
 //CLIENT-MODEL================================================================
 function validateItemForm()
 {
-// CODE
-if ($("#itemCode").val().trim() == "")
- {
- return "Insert Item Code.";
- }
-// NAME
-if ($("#itemName").val().trim() == "")
- {
- return "Insert Item Name.";
- }
-// PRICE-------------------------------
-if ($("#itemPrice").val().trim() == "")
- {
- return "Insert Item Price.";
- }
-// is numerical value
-var tmpPrice = $("#itemPrice").val().trim();
-if (!$.isNumeric(tmpPrice))
- {
- return "Insert a numerical value for Item Price.";
- }
-// convert to decimal price
- $("#itemPrice").val(parseFloat(tmpPrice).toFixed(2));
-// DESCRIPTION------------------------
-if ($("#itemDesc").val().trim() == "")
- {
- return "Insert Item Description.";
- }
-return true;
+	// CODE
+	if ($("#itemCode").val().trim() == "")
+	 {
+		return "Insert Item Code.";
+	 }
+	
+	// NAME
+	if ($("#itemName").val().trim() == "")
+	 {
+		return "Insert Item Name.";
+	 }
+	
+	// PRICE-------------------------------
+	if ($("#itemPrice").val().trim() == "")
+	 {
+		return "Insert Item Price.";
+	 }
+	
+	// is numerical value
+	var tmpPrice = $("#itemPrice").val().trim();
+	if (!$.isNumeric(tmpPrice))
+	 {
+		return "Insert a numerical value for Item Price.";
+	 }
+	
+	// convert to decimal price
+	 $("#itemPrice").val(parseFloat(tmpPrice).toFixed(2));
+	 
+	// DESCRIPTION------------------------
+	if ($("#itemDesc").val().trim() == "")
+	 {
+		return "Insert Item Description.";
+	 }
+	
+	return true;
 }
 
 
@@ -90,69 +99,70 @@ return true;
 
 function onItemSaveComplete(response, status)
 {
-if (status == "success")
- {
- var resultSet = JSON.parse(response);
- if (resultSet.status.trim() == "success")
- {
- $("#alertSuccess").text("Successfully saved.");
- $("#alertSuccess").show();
- $("#divItemsGrid").html(resultSet.data);
- } else if (resultSet.status.trim() == "error")
- {
- $("#alertError").text(resultSet.data);
- $("#alertError").show();
- }
- } else if (status == "error")
- {
- $("#alertError").text("Error while saving.");
- $("#alertError").show();
- } else
- {
- $("#alertError").text("Unknown error while saving..");
- $("#alertError").show();
- }
- $("#hidItemIDSave").val("");
- $("#formItem")[0].reset();
-}
+	if (status == "success")
+	 {
+		 var resultSet = JSON.parse(response);
+		 if (resultSet.status.trim() == "success")
+		 {
+		 $("#alertSuccess").text("Successfully saved.");
+		 $("#alertSuccess").show();
+		 $("#divItemsGrid").html(resultSet.data);
+		 } else if (resultSet.status.trim() == "error")
+		 {
+			 $("#alertError").text(resultSet.data);
+			 $("#alertError").show();
+		 }
+		 } else if (status == "error")
+		 {
+			 $("#alertError").text("Error while saving.");
+			 $("#alertError").show();
+		 } else
+		 {
+			 $("#alertError").text("Unknown error while saving..");
+			 $("#alertError").show();
+		 }
+		 $("#hidItemIDSave").val("");
+		 $("#formItem")[0].reset();
+	}
 
-$(document).on("click", ".btnRemove", function(event)
-		{
+	$(document).on("click", ".btnRemove", function(event)
+	{
 		 $.ajax(
 		 {
-		 url : "ItemsAPI",
-		 type : "DELETE",
-		 data : "itemID=" + $(this).data("itemid"),
-		 dataType : "text",
-		 complete : function(response, status)
+			 url : "ItemsAPI",
+			 type : "DELETE",
+			 data : "itemID=" + $(this).data("itemid"),
+			 dataType : "text",
+			 complete : function(response, status)
 		 {
-		 onItemDeleteComplete(response.responseText, status);
+			 onItemDeleteComplete(response.responseText, status);
 		 }
-		 });
-		});
+	 });
+});
 
 function onItemDeleteComplete(response, status)
 {
-if (status == "success")
- {
- var resultSet = JSON.parse(response);
- if (resultSet.status.trim() == "success")
- {
- $("#alertSuccess").text("Successfully deleted.");
- $("#alertSuccess").show();
- $("#divItemsGrid").html(resultSet.data);
- } else if (resultSet.status.trim() == "error")
- {
- $("#alertError").text(resultSet.data);
- $("#alertError").show();
- }
- } else if (status == "error")
- {
- $("#alertError").text("Error while deleting.");
- $("#alertError").show();
- } else
- {
- $("#alertError").text("Unknown error while deleting..");
- $("#alertError").show();
- }
+	if (status == "success")
+	 {
+		 var resultSet = JSON.parse(response);
+		 if (resultSet.status.trim() == "success")
+		 {
+			 $("#alertSuccess").text("Successfully deleted.");
+			 $("#alertSuccess").show();
+			 $("#divItemsGrid").html(resultSet.data);
+		 } else if (resultSet.status.trim() == "error")
+		 {
+			 $("#alertError").text(resultSet.data);
+			 $("#alertError").show();
+		 }
+	 } else if (status == "error")
+	 {
+		 $("#alertError").text("Error while deleting.");
+		 $("#alertError").show();
+	 } else
+	 {
+		 $("#alertError").text("Unknown error while deleting..");
+		 $("#alertError").show();
+	 }
+	 
 }
--- ClientApplication/src/com/ItemsAPI.java
+++ ClientApplication/src/com/ItemsAPI.java
@@ -15,60 +15,53 @@ import java.util.Scanner;
 public class ItemsAPI extends HttpServlet
 {
 	Item itemObj = new Item();
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			 throws ServletException, IOException
-			{
-			 String output = itemObj.insertItem(request.getParameter("itemCode"),
-			 request.getParameter("itemName"),
-			request.getParameter("itemPrice"),
-			request.getParameter("itemDesc"));
-			response.getWriter().write(output);
-			}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String output = itemObj.insertItem(request.getParameter("itemCode"),
+		request.getParameter("itemName"),
+		request.getParameter("itemPrice"),
+		request.getParameter("itemDesc"));
+		response.getWriter().write(output);
+	}
 	
 	// Convert request parameters to a Map
 	private static Map getParasMap(HttpServletRequest request)
 	{
-	 Map<String, String> map = new HashMap<String, String>();
-	try
-	 {
-	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
-	 String queryString = scanner.hasNext() ?
-	 scanner.useDelimiter("\\A").next() : "";
-	 scanner.close();
-	 String[] params = queryString.split("&");
-	 for (String param : params)
-	 { 
-		 String[] p = param.split("=");
-		 map.put(p[0], p[1]);
-		 }
-		 }
-		catch (Exception e)
+		 Map<String, String> map = new HashMap<String, String>();
+		 try
 		 {
-		 }
-		return map;
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			 String queryString = scanner.hasNext() ?
+			 scanner.useDelimiter("\\A").next() : "";
+			 scanner.close();
+			 String[] params = queryString.split("&");
+			 for (String param : params)
+			 { 
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]);
+			 }
+		 }catch (Exception e){}
+			
+		 return map;
 		
 	 }
 	
-	protected void doPut(HttpServletRequest request, HttpServletResponse response)
-			 throws ServletException, IOException
-			{
-			 Map paras = getParasMap(request);
-			 String output = itemObj.updateItem(paras.get("hidItemIDSave").toString(),
-			 paras.get("itemCode").toString(),
-			 paras.get("itemName").toString(),
-			paras.get("itemPrice").toString(),
-			paras.get("itemDesc").toString());
-			response.getWriter().write(output);
-			}
-	
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
-	 throws ServletException, IOException
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-	 Map paras = getParasMap(request);
-	 String output = itemObj.deleteItem(paras.get("itemID").toString());
-	response.getWriter().write(output);
+		 Map paras = getParasMap(request);
+		 String output = itemObj.updateItem(paras.get("hidItemIDSave").toString(),
+		 paras.get("itemCode").toString(),
+		 paras.get("itemName").toString(),
+		 paras.get("itemPrice").toString(),
+		 paras.get("itemDesc").toString());
+		 response.getWriter().write(output);
 	}
 	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		 Map paras = getParasMap(request);
+		 String output = itemObj.deleteItem(paras.get("itemID").toString());
+		 response.getWriter().write(output);
+	}
 	
-
 }
