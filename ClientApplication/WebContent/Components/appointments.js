df2621e34d4fe99940c39fc036433ec97dd9aa91
rjayram96 <rjayram96@gmail.com>
2020-05-06 23:48:37 +0530

    minor fixes 2

--- ClientApplication/WebContent/Components/appointments.js
+++ ClientApplication/WebContent/Components/appointments.js
@@ -7,34 +7,6 @@ $(document).ready(function()
 	 $("#alertError").hide();
 });
 
-function onItemSaveComplete(response, status)
-{
-	if (status == "success")
-	 {
-		 var resultSet = JSON.parse(response);
-		 if (resultSet.status.trim() == "success")
-		 {
-		 $("#alertSuccess").text("Successfully saved.");
-		 $("#alertSuccess").show();
-		 $("#divItemsGrid").html(resultSet.data);
-		 } else if (resultSet.status.trim() == "error")
-		 {
-			 $("#alertError").text(resultSet.data);
-			 $("#alertError").show();
-		 }
-	 } else if (status == "error")
-	 {
-		 $("#alertError").text("Error while saving.");
-		 $("#alertError").show();
-	 } else
-	 {
-		 $("#alertError").text("Unknown error while saving..");
-		 $("#alertError").show();
-	 }
-	 $("#hidItemIDSave").val("");
-	 $("#formItem")[0].reset();
-}
-
 //SAVE ============================================
 $(document).on("click", "#btnSave", function(event)
 {
@@ -71,6 +43,78 @@ $(document).on("click", "#btnSave", function(event)
 	
 	  
  });
+
+function onItemSaveComplete(response, status)
+{
+	if (status == "success")
+	 {
+		 var resultSet = JSON.parse(response);
+		 if (resultSet.status.trim() == "success")
+		 {
+		 $("#alertSuccess").text("Successfully saved.");
+		 $("#alertSuccess").show();
+		 $("#divItemsGrid").html(resultSet.data);
+		 } else if (resultSet.status.trim() == "error")
+		 {
+			 $("#alertError").text(resultSet.data);
+			 $("#alertError").show();
+		 }
+	 } else if (status == "error")
+	 {
+		 $("#alertError").text("Error while saving.");
+		 $("#alertError").show();
+	 } else
+	 {
+		 $("#alertError").text("Unknown error while saving..");
+		 $("#alertError").show();
+	 }
+	 $("#hidItemIDSave").val("");
+	 $("#formItem")[0].reset();
+}
+
+$(document).on("click", ".btnRemove", function(event)
+		{
+			 $.ajax(
+			 {
+				 url : "AppointmentsAPI",
+				 type : "DELETE",
+				 data : "appId=" + $(this).data("appid"),
+				 dataType : "text",
+				 complete : function(response, status)
+				 {
+					 onItemDeleteComplete(response.responseText, status);
+				 }
+			 });
+		});
+
+function onItemDeleteComplete(response, status)
+{
+	if (status == "success")
+	 {
+		 var resultSet = JSON.parse(response);
+		 if (resultSet.status.trim() == "success")
+		 {
+			 $("#alertSuccess").text("Successfully deleted.");
+			 $("#alertSuccess").show();
+			 $("#divItemsGrid").html(resultSet.data);
+		 } else if (resultSet.status.trim() == "error")
+		 {
+			 $("#alertError").text(resultSet.data);
+			 $("#alertError").show();
+		 }
+	 } else if (status == "error")
+	 {
+		 $("#alertError").text("Error while deleting.");
+		 $("#alertError").show();
+	 } else
+	 {
+		 $("#alertError").text("Unknown error while deleting..");
+		 $("#alertError").show();
+	 }
+	 
+}
+
+
 
 
 
@@ -131,45 +175,6 @@ function validateItemForm()
 }
 
 
-function onItemDeleteComplete(response, status)
-{
-	if (status == "success")
-	 {
-		 var resultSet = JSON.parse(response);
-		 if (resultSet.status.trim() == "success")
-		 {
-			 $("#alertSuccess").text("Successfully deleted.");
-			 $("#alertSuccess").show();
-			 $("#divItemsGrid").html(resultSet.data);
-		 } else if (resultSet.status.trim() == "error")
-		 {
-			 $("#alertError").text(resultSet.data);
-			 $("#alertError").show();
-		 }
-	 } else if (status == "error")
-	 {
-		 $("#alertError").text("Error while deleting.");
-		 $("#alertError").show();
-	 } else
-	 {
-		 $("#alertError").text("Unknown error while deleting..");
-		 $("#alertError").show();
-	 }
-	 
-}
 
-$(document).on("click", ".btnRemove", function(event)
-{
-	 $.ajax(
-	 {
-		 url : "AppointmentsAPI",
-		 type : "DELETE",
-		 data : "appid=" + $(this).data("appid"),
-		 dataType : "text",
-		 complete : function(response, status)
-		 {
-			 onItemDeleteComplete(response.responseText, status);
-		 }
-	 });
-});
+
 
